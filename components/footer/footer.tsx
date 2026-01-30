196be95ee6b015eb8ae5b9fc85887ed759adb613
PenguinLogistic <ryan.shiuhong.fung@gmail.com>
2026-01-30 12:53:15 -0500

    mobile footer fixes

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -24,69 +24,70 @@ export default function Footer() {
   // // }, [pathname]);
 
   return (
-    <div className="flex flex-col bg-blue-midnight gap-20 sm:p-20">
-      <section>
-        <div className="flex w-full justify-center">
-          <div className="flex flex-col gap-3 mx-auto justify-center">
-            <Heading variant="h2" className="text-white">
-              Ryan Fung
-            </Heading>
-            <Text variant="small" className="text-white">
-              Toronto, Canada &#8226; &copy; 2026-2027 Ryan Fung
-            </Text>
-          </div>
+    <section className="flex flex-col bg-blue-midnight gap-20 p-10 sm:p-20">
+      <div className="flex flex-col w-full justify-center gap-6 sm:gap-0 sm:flex-row">
+        <div className="flex flex-col gap-3 mx-auto justify-center">
+          <Heading
+            variant="h2"
+            className="self-center sm:self-start text-white"
+          >
+            Ryan Fung
+          </Heading>
+          <Text variant="small" className="text-white">
+            Toronto, Canada &#8226; &copy; 2026-2027 Ryan Fung
+          </Text>
+        </div>
 
-          {/* <div className="flex flex-col items-center text-center justify-center">
+        {/* <div className="flex flex-col items-center text-center justify-center">
             <Text className="text-white max-w-sm">&quot;{quote}&quot;</Text>
           </div> */}
 
-          <div className="flex gap-4 items-start mx-auto flex-col">
-            <div className="flex items-center ">
-              <Button asChild variant="invisible" className="p-0">
-                <Link
-                  href="mailto: ryan.shiuhong.fung@gmail.com"
-                  className="flex gap-4"
-                >
-                  <RiMailAddFill className="text-white" />
-                  <Text variant="small" className="text-white hover:underline">
-                    ryan.shiuhong.fung@gmail.com
-                  </Text>
-                </Link>
-              </Button>
-            </div>
-            <div className="flex items-center gap-4">
-              <Button asChild variant="invisible" className="p-0">
-                <Link
-                  href="https://github.com/PenguinLogistic/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex gap-4"
-                >
-                  <RiGithubFill className="text-white" />
-                  <Text variant="small" className="text-white hover:underline">
-                    https://github.com/PenguinLogistic/
-                  </Text>
-                </Link>
-              </Button>
-            </div>
-            <div className="flex items-center gap-4">
-              <Button asChild variant="invisible" className="p-0">
-                <Link
-                  href="https://www.linkedin.com/in/ryan-sh-fung/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex gap-4"
-                >
-                  <RiLinkedinBoxFill className="text-white" />
-                  <Text variant="small" className="text-white hover:underline">
-                    https://www.linkedin.com/in/ryan-sh-fung/
-                  </Text>
-                </Link>
-              </Button>
-            </div>
+        <div className="flex gap-4 items-center sm:items-start mx-auto flex-col">
+          <div className="flex items-center">
+            <Button asChild variant="invisible" className="p-0">
+              <Link
+                href="mailto: ryan.shiuhong.fung@gmail.com"
+                className="flex gap-4"
+              >
+                <RiMailAddFill className="text-white" />
+                <Text variant="small" className="text-white hover:underline">
+                  ryan.shiuhong.fung@gmail.com
+                </Text>
+              </Link>
+            </Button>
+          </div>
+          <div className="flex items-center gap-4">
+            <Button asChild variant="invisible" className="p-0">
+              <Link
+                href="https://github.com/PenguinLogistic/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex gap-4"
+              >
+                <RiGithubFill className="text-white" />
+                <Text variant="small" className="text-white hover:underline">
+                  https://github.com/PenguinLogistic/
+                </Text>
+              </Link>
+            </Button>
+          </div>
+          <div className="flex items-center gap-4">
+            <Button asChild variant="invisible" className="p-0">
+              <Link
+                href="https://www.linkedin.com/in/ryan-sh-fung/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex gap-4"
+              >
+                <RiLinkedinBoxFill className="text-white" />
+                <Text variant="small" className="text-white hover:underline">
+                  https://www.linkedin.com/in/ryan-sh-fung/
+                </Text>
+              </Link>
+            </Button>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 }
